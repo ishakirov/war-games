02e9a8faaeab6cc3f714f1308e84efad9c13c87c
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2016-03-17 10:28:28 +0100

    cleaning unused things in the peer.py

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
-# P2PSP War Games #
+# P2PSP War Games
 
 Test bed to measure the impact of several types of attacks in a P2PSP Team.
 
-## Pre-requisites ##
-### Linux ###
+## Cloning
+To clone this projects with all submodules, make shure you
+clone with `--recursive` argument: `git clone --recursive ...`
+
+## Pre-requisites
+### Linux
 ```
 $ [sudo] apt-get install libboost-all-dev
 ```
 
+## Build the P2PSP Library
+### Linux
+```
+$ cd lib/p2psp
+$ ./make.py
+$ cd ../..
+```
+
+## Run it
+
+TO-DO (working on a script for the simulation)
--- peer.py
+++ peer.py
@@ -34,20 +34,6 @@ from _print_ import _print_
 sys.path.append('lib/p2psp/bin/')
 import libp2psp
 
-#from core.peer_ims import Peer_IMS
-#from core.peer_dbs import Peer_DBS
-#from core.lossy_socket import lossy_socket
-#from core.symsp_peer import Symsp_Peer
-#from core.monitor_dbs import Monitor_DBS
-#from core.monitor_lrs import Monitor_LRS
-#from core.monitor_nts import Monitor_NTS
-#from core.peer_nts import Peer_NTS
-#from core.lossy_peer import Lossy_Peer
-#from core.peer_strpeds import Peer_StrpeDs
-#from core.malicious_peer import MaliciousPeer # Other dir, maybe "DIS" ??
-#from core.peer_strpeds_malicious import Peer_StrpeDsMalicious
-#from core.trusted_peer import TrustedPeer
-
 # }}}
 
 # Some useful definitions.
@@ -76,12 +62,12 @@ class Peer():
         parser = argparse.ArgumentParser(description='This is the peer node of a P2PSP team.')
 
         parser.add_argument('--enable_chunk_loss', help='Forces a lost of chunks')
-        parser.add_argument('--max_chunk_debt', help='The maximun number of times that other peer can not send a chunk to this peer. Defaut = {}')#.format(Peer_DBS.MAX_CHUNK_DEBT))
-        parser.add_argument('--player_port', help='Port to communicate with the player. Default = {}')#.format(Peer_IMS.PLAYER_PORT))
+        parser.add_argument('--max_chunk_debt', help='The maximun number of times that other peer can not send a chunk to this peer. Defaut = {}'.format(peer.max_chunk_debt))
+        parser.add_argument('--player_port', help='Port to communicate with the player. Default = {}'.format(peer.player_port))
         parser.add_argument('--port_step', help='Source port step forced when behind a sequentially port allocating NAT (conflicts with --chunk_loss_period). Default = {}')#.format(Symsp_Peer.PORT_STEP))
-        parser.add_argument('--splitter_addr', help='IP address or hostname of the splitter. Default = {}.')#.format(Peer_IMS.SPLITTER_ADDR))
-        parser.add_argument('--splitter_port', help='Listening port of the splitter. Default = {}.')#.format(Peer_IMS.SPLITTER_PORT))
-        parser.add_argument('--port', help='Port to communicate with the peers. Default {} (the OS will chose it).')#.format(Peer_IMS.PORT))
+        parser.add_argument('--splitter_addr', help='IP address or hostname of the splitter. Default = {}.'.format(peer.splitter_addr))
+        parser.add_argument('--splitter_port', help='Listening port of the splitter. Default = {}.'.format(peer.splitter_port))
+        parser.add_argument('--port', help='Port to communicate with the peers. Default {} (the OS will chose it).'.format(peer.port))
         parser.add_argument('--use_localhost', action="store_true", help='Forces the peer to use localhost instead of the IP of the adapter to connect to the splitter. Notice that in this case, peers that run outside of the host will not be able to communicate with this peer.')
         parser.add_argument('--malicious', action="store_true", help='Enables the malicious activity for peer.')
         parser.add_argument('--persistent', action="store_true", help='Forces the peer to send poisoned chunks to other peers.')
@@ -99,7 +85,6 @@ class Peer():
         except Exception:
             pass
 
-        #args = parser.parse_known_args()[0]
         args = parser.parse_args()
 
         if args.splitter_addr:
@@ -126,7 +111,6 @@ class Peer():
             peer.use_localhost = True
             _print_('Using localhost address')
 
-        #peer = Peer_IMS()
         peer.WaitForThePlayer()
         peer.ConnectToTheSplitter()
         peer.ReceiveTheMcasteEndpoint()
@@ -143,7 +127,6 @@ class Peer():
         if peer.mcast_addr == "0.0.0.0":
             # {{{ IP unicast mode.
 
-            #peer = Peer_DBS(peer)
             _print_("Peer DBS enabled")
             peer.ReceiveMyEndpoint()
             peer.ReceiveMagicFlags()
@@ -163,42 +146,12 @@ class Peer():
                 _print_("Monitor DBS enabled")
 
                 # The peer is a monitor. Now it's time to know the sets of rules that control this team.
-                '''
-                if (peer.magic_flags & Common.LRS):
-                    peer = Peer_LRS(peer)
-                    _print_("Peer LRS enabled")
-                    peer = Monitor_LRS(peer)
-                    _print_("Monitor LRS enabled")
-                if (peer.magic_flags & Common.NTS):
-                    peer = Peer_NTS(peer)
-                    _print_("Peer NTS enabled")
-                    peer = Monitor_NTS(peer)
-                    _print_("Monitor NTS enabled")
-                '''
             else:
                 _print_("Peer DBS enabled")
                 # The peer is a normal peer. Let's know the sets of rules that control this team.
-                '''
-                if (peer.magic_flags & Common.NTS):
-                    peer = Peer_NTS(peer)
-                    _print_("Peer NTS enabled")
-
-                if args.enable_chunk_loss:
-
-                    if args.chunk_loss_period:
-                        Lossy_Peer.CHUNK_LOSS_PERIOD = int(args.chunk_loss_period)
-                        print('CHUNK_LOSS_PERIOD =', Lossy_Peer.CHUNK_LOSS_PERIOD)
-                        if int(args.chunk_loss_period) != 0:
-                            peer = Lossy_Peer(peer)
-                            _print_("Lost of chunks enabled")
-                '''
+               
             '''
-            if args.port_step:
-                Symsp_Peer.PORT_STEP = int(args.port_step)
-                print('PORT_STEP =', Symsp_Peer.PORT_STEP)
-                if int(args.port_step) != 0:
-                    peer = Symsp_Peer(peer)
-
+           
             if args.strpeds:
                 peer = Peer_StrpeDs(peer)
                 peer.receive_dsa_key()
@@ -263,17 +216,8 @@ class Peer():
         last_chunk_number = peer.GetPlayedChunk()
         peer.sendto_counter = 0
         last_sendto_counter = 0
-
-        '''        
-	if hasattr(peer, 'sendto_counter'):
-            last_sendto_counter = 0
-        else:
-            peer.sendto_counter = 0
-            last_sendto_counter = 0
-        if not hasattr(peer, 'peer_list'):
-            peer.peer_list = []
-        '''
         last_recvfrom_counter = peer.recvfrom_counter
+        
         while peer.IsPlayerAlive():
             time.sleep(1)
             kbps_expected_recv = ((peer.GetPlayedChunk() - last_chunk_number) * peer.chunk_size * 8) / 1000
@@ -285,21 +229,6 @@ class Peer():
             kbps_sendto = ((peer.sendto_counter - last_sendto_counter) * peer.chunk_size * 8) / 1000
             last_sendto_counter = peer.sendto_counter
             
-            '''
-            try:
-                if Common.CONSOLE_MODE == False :
-                    from gi.repository import GObject
-                    try:
-                        from adapter import speed_adapter
-                    except ImportError as msg:
-                        pass
-                    GObject.idle_add(speed_adapter.update_widget,str(kbps_recvfrom) + ' kbps'
-                                            ,str(kbps_sendto) + ' kbps'
-                                            ,str(len(peer.peer_list)+1))
-            except Exception as msg:
-                pass
-            '''
-
             if kbps_recvfrom > 0 and kbps_expected_recv > 0:
                 nice = 100.0/float((float(kbps_expected_recv)/kbps_recvfrom)*(len(peer.GetPeerList())+1))
             else:
