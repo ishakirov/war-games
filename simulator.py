f7748c27c7d5dc02ecdfd6e56f89695013a951d9
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2016-07-11 17:52:58 +0200

    testing

--- simulator.py
+++ simulator.py
@@ -44,7 +44,7 @@ P_MP = 100
 P_MPL = 100
 P_TPL = 100
 MPTR = 5
-WACLR_max = 0.10
+WACLR_max = 1.
 WACLR_max_var = 1.
 alpha = 0.25
 WEIBULL_SHAPE = 1.
@@ -80,7 +80,7 @@ def runStream():
 def runSplitter(ds = False):
     prefix = ""
     if ds: prefix = "ds"
-    run("./console/bin/splitter --strpeds --team_port 8001 --source_port 8080 --max_number_of_chunk_loss 32 --chunk_size 256 --buffer_size 1024 --strpeds_log " + experiment_path + "/splitter.log --p_mpl " + str(P_MPL) + " --p_tpl " + str(P_TPL), open("{0}/splitter.out".format(experiment_path), "w"))
+    run("./console/bin/splitter --strpeds --team_port 8001 --source_port 8080 --max_number_of_chunk_loss 32 --chunk_size 512 --buffer_size 1024 --strpeds_log " + experiment_path + "/splitter.log --p_mpl " + str(P_MPL) + " --p_tpl " + str(P_TPL), open("{0}/splitter.out".format(experiment_path), "w"))
     time.sleep(0.5)
 
 def runPeer(trusted = False, malicious = False, ds = False):
@@ -305,7 +305,7 @@ def addRegularOrMaliciousPeer():
     if round > 0:
         WACLR_max_var = WACLR_max + (1/(round/(round + 100.))) - 1
 
-    progress ="Round "+ str(round)+" Size "+str(sizeTeam)+"/"+str(nPeersTeam)
+    progress ="Round "+ str(round)+" Size "+str(nPeersTeam)+"/"+str(sizeTeam)
     sys.stdout.flush()
     print progress,
     print str(int(time.time()-INIT_TIME))+"/"+str(TOTAL_TIME),
